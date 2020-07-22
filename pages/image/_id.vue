3ca9e87378568df602ca7b6f2b5af1030750c61a
Josh Donnell <josh@josh.donnell.co.uk>
2020-07-22 22:25:35 +0100

    Add profile to the single page

--- pages/image/_id.vue
+++ pages/image/_id.vue
@@ -3,18 +3,20 @@
       <img :src="post.image" alt="">
       <span v-for="comment in comments" :key="comment.ID">{{ comment.comment }}</span>
       <div>You have {{likes}} like</div>
+      <nuxt-link v-if="user" :to="'/profile/' + user.username">{{ user.username }}</nuxt-link>
   </div>
 </template>
 
 <script>
-import { postsCollection, commentsCollection, likesCollection } from '@/plugins/firebase'
+import { postsCollection, commentsCollection, likesCollection, usersCollection } from '@/plugins/firebase'
 
 export default {
     data() {
         return {
             post: {},
             comments: [],
-            likes: 0
+            likes: 0,
+            user: {}
         }
     },
 
@@ -32,7 +34,15 @@ export default {
         await likesCollection.where("post-id", "==", this.$route.params.id).get().then((likes) => { 
             this.likes = likes.size
         })
-    }
+
+        if (this.post['user-id']) {
+            let users = await usersCollection.where("id", "==", this.post['user-id']).get()
+            await users.forEach(user => {
+                this.user = user.data()
+            });
+        }
+
+    },
 }
 </script>
 
